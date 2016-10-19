71069d425749cb7a4cb144aafaece0e720a1b42e
Dave Mateer <davemateer@gmail.com>
2016-10-19 17:01:24 +0100

    Auto commit at 19/10/2016 17:01

---
+++ ThinkBooksWebsite/ThinkBooksWebsite/Controllers/AuthorsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThinkBooksWebsite.Services;
+
+namespace ThinkBooksWebsite.Controllers
+{
+    public class Author
+    {
+        public int AuthorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+
+    public class AuthorsController : Controller
+    {
+        // GET: Authors - the default view too ie /
+        public ActionResult Index()
+        {
+            var db = new AuthorRepository();
+            List<Author> authors = db.GetAuthors();
+            return View(authors);
+        }
+    }
+}
