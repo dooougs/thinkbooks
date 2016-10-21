03c3490c19f951dc81501a1618cac705392b0d4d
Dave Mateer <davemateer@gmail.com>
2016-10-21 12:26:15 +0100

    got sorting working with SP and UI. Looking at inline SQL version

--- ThinkBooksWebsite/ThinkBooksWebsite/Controllers/AuthorsController.cs
+++ ThinkBooksWebsite/ThinkBooksWebsite/Controllers/AuthorsController.cs
@@ -7,11 +7,17 @@ namespace ThinkBooksWebsite.Controllers
 {
     public class AuthorsController : Controller
     {
-        // GET: Authors - the default view too ie /
-        public ActionResult Index(string sortOrder = "LastName", string sortDirection = "ASC")
+        public ActionResult Index(string s = "LastName")
         {
+            var sortColumnAndDirection = s;
             var db = new AuthorsRepository();
-            List<Author> authors = db.GetAuthors(sortOrder, sortDirection);
+            List<Author> authors = db.GetAuthors(sortColumnAndDirection);
+
+            // Flip the order of the sort param on the button so next time it is pressed with reverse current
+            ViewBag.SortParamAuthorID = sortColumnAndDirection == "AuthorID" ? "AuthorID_desc" : "AuthorID";
+            ViewBag.SortParamFirstName = sortColumnAndDirection == "FirstName" ? "FirstName_desc" : "FirstName";
+            ViewBag.SortParamLastName = sortColumnAndDirection == "LastName" ? "LastName_desc" : "LastName";
+            ViewBag.SortParamDateOfBirth = sortColumnAndDirection == "DateOfBirth" ? "DateOfBirth_desc" : "DateOfBirth";
             return View(authors);
         }
     }
--- ThinkBooksWebsite/ThinkBooksWebsite/Services/AuthorsRepository.cs
+++ ThinkBooksWebsite/ThinkBooksWebsite/Services/AuthorsRepository.cs
@@ -1,38 +1,66 @@
 ﻿using Dapper;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using ThinkBooksWebsite.Models;
 
 namespace ThinkBooksWebsite.Services
 {
-    //public enum AuthorSortDirection
-    //{
-    //    AuthorID,
-    //    FirstName, 
-    //    LastName
-    //}
-
     public class AuthorsRepository
     {
-        public List<Author> GetAuthors(string sortColumn, string sortDirection)
+        // Stored Proc
+        //public List<Author> GetAuthors(string sortColumnAndDirection)
+        //{
+        //    using (var db = Util.GetOpenConnection())
+        //    {
+        //        var p = new DynamicParameters();
+        //        var sortDirection = "ASC";
+        //        var sortColumn = sortColumnAndDirection;
+        //        if (sortColumnAndDirection.EndsWith("_desc"))
+        //        {
+        //            sortDirection = "DESC";
+        //            sortColumn = sortColumn.Substring(0, sortColumnAndDirection.Length - 5);
+        //        }
+        //        p.Add("@SortColumn", sortColumn);
+        //        p.Add("@SortDirection", sortDirection);
+        //        return db.Query<Author>("GetAuthors", p, commandType: CommandType.StoredProcedure).ToList();
+        //    }
+        //}
+
+        // Using inline SQL
+        public List<Author> GetAuthors(string sortColumnAndDirection)
         {
             using (var db = Util.GetOpenConnection())
             {
-                // {=} is inline value injection, but has to be an int, or enum
-                //SELECT TOP {= count} *FROM Author WHERE FirstName LIKE { @firstName} ORDER BY {= sortOrder}
+                var sortDirection = "ASC";
+                var sortColumn = sortColumnAndDirection;
+                if (sortColumnAndDirection.EndsWith("_desc"))
+                {
+                    sortDirection = "DESC";
+                    sortColumn = sortColumn.Substring(0, sortColumnAndDirection.Length - 5);
+                }
 
-                // could do this but SQL injection issue!
-                //"SELECT * FROM Author ORDER BY " + sortOrder
+                // Making sure it can only be set to DESC or ASC
+                string sanitizedSortDirection = sortDirection == "DESC" ? "DESC" : "ASC";
 
-                // can you do SQL injection with a SP?
-                //return db.Query<Author>("SELECT * FROM Author ORDER BY " +  sortOrder, new { sortOrder = AuthorSortDirection.LastName, sortDirection }).ToList();
+                DbCommandBuilder commandBuilder = new SqlCommandBuilder();
+                string sanitizedSortColumn = commandBuilder.QuoteIdentifier(sortColumn);
 
-                var p = new DynamicParameters();
-                p.Add("@SortColumn", sortColumn);
-                p.Add("@SortDirection", sortDirection);
-                return db.Query<Author>("GetAuthors", p, commandType: CommandType.StoredProcedure).ToList();
+                var sql = "SELECT * FROM Author WHERE FirstName LIKE CONCAT('%',@firstName,'%') ORDER BY " + sanitizedSortColumn + " " + sanitizedSortDirection;
+                return db.Query<Author>(sql, new { firstName = "Ali"}).ToList();
             }
         }
+
+        // {=} is inline value injection, but has to be an int, or enum
+        //SELECT TOP {= count} *FROM Author WHERE FirstName LIKE { @firstName} ORDER BY {= sortOrder}
+
+        // could do this but SQL injection issue!
+        //"SELECT * FROM Author ORDER BY " + sortOrder
+
+        // can you do SQL injection with a SP?
+        //return db.Query<Author>("SELECT * FROM Author ORDER BY " +  sortOrder, new { sortOrder = AuthorSortDirection.LastName, sortDirection }).ToList();
+
     }
 }
