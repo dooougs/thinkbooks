0acbb77a989e7a43bc0b0997dc718148b2e301ea
Dave Mateer <davemateer@gmail.com>
2016-10-26 11:45:17 +0100

    Auto commit at 26/10/2016 11:45

---
+++ ThinkBooksDB/dbo/Tables/Book.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Book] (
+    [BookID]   INT            IDENTITY (1, 1) NOT NULL,
+    [AuthorID] INT            NOT NULL,
+    [Title]    NVARCHAR (255) NOT NULL,
+    CONSTRAINT [PK_Book] PRIMARY KEY CLUSTERED ([BookID] ASC)
+);
+
