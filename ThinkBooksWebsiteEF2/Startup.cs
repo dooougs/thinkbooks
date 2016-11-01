2c3b01ade99f239d7d7c689658be4141e62cd3d9
Dave Mateer <davemateer@gmail.com>
2016-11-01 07:57:51 +0000

    Auto commit at 01/11/2016 7:57

---
+++ ThinkBooksWebsiteEF2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThinkBooksWebsiteEF2.Startup))]
+namespace ThinkBooksWebsiteEF2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
