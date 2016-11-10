81e67e6170ab76f804a977572e8fdb35cea9729e
Dave Mateer <davemateer@gmail.com>
2016-11-10 06:34:16 +0000

    Auto commit at 10/11/2016 6:34

--- ThinkBooksWebsite/Services/BooksRepository.cs
+++ ThinkBooksWebsite/Services/BooksRepository.cs
@@ -13,76 +13,11 @@ namespace ThinkBooksWebsite.Services
 
     public class BooksRepository
     {
-        //    public BooksViewModel GetBooks(string sortColumnAndDirection, int numberOfResults, int currentPage, int? bookIDFilter,
-        //        string titleFilter)
         public BooksViewModel GetBooks()
         {
             using (var db = Util.GetOpenConnection())
             {
-                //var sortDirection = "ASC";
-                //var sortColumn = sortColumnAndDirection;
-                //if (sortColumnAndDirection.EndsWith("_desc"))
-                //{
-                //    sortDirection = "DESC";
-                //    sortColumn = sortColumn.Substring(0, sortColumnAndDirection.Length - 5);
-                //}
-
-                //var commandBuilder = new SqlCommandBuilder();
-                //var sanitizedSortColumn = commandBuilder.QuoteIdentifier(sortColumn);
-
-                //var offset = (currentPage - 1) * numberOfResults;
-                var sql = "SELECT top 10 * FROM Book";
-                //var sql = @"
-                //    SELECT * FROM Author 
-                //    WHERE (@AuthorID IS NULL OR AuthorID = @AuthorID)
-                //    AND (@FirstName IS NULL OR FirstName LIKE CONCAT('%',@FirstName,'%'))
-                //    AND (@LastName IS NULL OR LastName LIKE '%'+@LastName+'%')
-                //    AND (@DateOfBirth IS NULL OR DateOfBirth = @DateOfBirth)
-                //    ORDER BY " + sanitizedSortColumn + " " + sortDirection + @"
-                //    OFFSET "+ offset + @" ROWS 
-                //    FETCH NEXT " + numberOfResults + " ROWS ONLY";
-                var result = db.Query<Book>(sql).ToList();
-                //var result = db.Query<Author>(sql,
-                //         new
-                //         {
-                //             AuthorID = authorIDFilter,
-                //             FirstName = firstNameFilter,
-                //             LastName = lastNameFilter,
-                //             DateOfBirth = dateOfBirthFilter,
-                //             numberOfResults,
-                //             PageNumber = currentPage
-                //         }).ToList();
-
-
-                //// seems super fast doing 2 queries ie don't need to do return multiple record sets
-                //string sqlCount;
-                //if (authorIDFilter == null && dateOfBirthFilter == null && firstNameFilter.IsNullOrWhiteSpace() && lastNameFilter.IsNullOrWhiteSpace())
-                //{
-                //    sqlCount = @"SELECT SUM(p.rows)
-                //          FROM sys.partitions AS p
-                //          INNER JOIN sys.tables AS t
-                //          ON p.[object_id] = t.[object_id]
-                //          INNER JOIN sys.schemas AS s
-                //          ON t.[schema_id] = s.[schema_id]
-                //          WHERE p.index_id IN (0,1) -- heap or clustered index
-                //          AND t.name = N'Author'
-                //          AND s.name = N'dbo'";
-                //}
-                //else
-                //{
-                //    sqlCount = @"SELECT COUNT(*) FROM Author
-                //                WHERE(@AuthorID IS NULL OR AuthorID = @AuthorID)
-                //                AND(@firstName IS NULL OR FirstName LIKE CONCAT('%', @firstName, '%'))
-                //                AND(@LastName IS NULL OR LastName LIKE '%' + @LastName + '%')
-                //                AND(@DateOfBirth IS NULL OR DateOfBirth = @DateOfBirth)";
-                //}
-                //var count = db.Query<int>(sqlCount, new
-                //{
-                //    authorID = authorIDFilter,
-                //    firstName = firstNameFilter,
-                //    lastName = lastNameFilter,
-                //    dateOfBirth = dateOfBirthFilter
-                //}).Single();
+                var result = db.Query<Book>("SELECT TOP 10 * FROM Book").ToList();
 
                 var vm = new BooksViewModel
                 {
