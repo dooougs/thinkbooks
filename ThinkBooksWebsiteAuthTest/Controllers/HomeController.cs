b54ceb40e335149e7adae8e118bc67dfa8724181
Dougal Kennedy <Dougal.Kennedy@qnrl.com>
2016-12-07 11:12:57 +0000

    Added base class which forces Authorize attribute. Inherit from MVC Authorize and create local namespace version which properly handles 401/403 differences and also checks Roles

--- ThinkBooksWebsiteAuthTest/Controllers/HomeController.cs
+++ ThinkBooksWebsiteAuthTest/Controllers/HomeController.cs
@@ -1,47 +1,93 @@
 ﻿using System;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace ThinkBooksWebsiteAuthTest.Controllers
 {
     // ThinkBooks Role
     public static class TR
     {
-        public static string All = "QNRL\\All Quorum";
-        public static string Dev = "QNRL\\Developers";
-        public static string Admin = "QNRL\\Administrators";
+        public const string All = "QNRL\\All Quorum";
+        public const string Dev = "QNRL\\Developers";
+        public const string Admin = "QNRL\\Administrators";
+        public const string or = ",";
 
         public static bool AuthOn = Convert.ToBoolean(System.Configuration.ConfigurationManager.AppSettings["AuthOn"]);
     }
 
-    public class HomeController : Controller
+    [Authorize]
+    public class TBController : Controller { }
+
+    public class HomeController : TBController
     {
         public ActionResult Index()
         {
             return View();
         }
 
+        [Authorize(Roles = TR.Dev+TR.or+TR.Admin )]
         public ActionResult About()
         {
-            // If Auth is on (in web.config), specifices which roles the user needs to be in to run this method
-            if (TR.AuthOn)
-                if (User.IsInRole(TR.Dev) || User.IsInRole(TR.Admin)) { }
-                else return RedirectToAction("Unauthorized", "Errors", null);
-
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
+        [Authorize(Roles = TR.Admin)]
         public ActionResult Contact()
         {
-            // Specifices which roles need to be in to run this method
-            if (TR.AuthOn)
-                if (User.IsInRole(TR.Admin)) { }
-                else return RedirectToAction("Unauthorized", "Errors", null);
-
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class AuthorizeAttribute : System.Web.Mvc.AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (TR.AuthOn)
+            {
+                bool pass = false;
+
+                TBController ctrl = filterContext.Controller as TBController;
+                if (Roles != String.Empty)
+                {
+                    foreach (var role in Roles.Split(TR.or.ToCharArray()))
+                    {
+                        if (pass = ctrl.User.IsInRole(role)) // deliberate assignment
+                            break;
+                    }
+                }
+                else
+                {
+                    pass = ctrl.User.IsInRole(TR.All);
+                }
+                if (!pass)
+                {
+                    filterContext.Result = new RedirectToRouteResult(
+                                            new RouteValueDictionary
+                                            {
+                                            { "action", "Unauthorized" },
+                                            { "controller", "Errors" }
+                                            });
+                    return;
+                }
+                base.OnAuthorization(filterContext);
+            }
+
+        }
+        protected override void HandleUnauthorizedRequest(System.Web.Mvc.AuthorizationContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.IsAuthenticated)
+            {
+                filterContext.Result = new System.Web.Mvc.HttpStatusCodeResult((int)System.Net.HttpStatusCode.Forbidden);
+            }
+            else
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
         }
     }
 }
